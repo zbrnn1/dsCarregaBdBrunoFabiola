e5222d105c062519b35e850294af2da2e2d60b72
BRUNO ECHEVERRIA DA CUNHA  TnolADS <bruno.cunha2@estudante.ifms.edu.br>
2023-03-17 20:29:47 -0400

    repositorio Bruno e Fabiola

--- src/main/java/carregabanco/CarregaBanco.java
+++ src/main/java/carregabanco/CarregaBanco.java
@@ -5,6 +5,9 @@ import carregabanco.view.CarregaBancoView;
 public class CarregaBanco {
 	public static void main(String[] args) {
 		new CarregaBancoView();
-		//AlunoModel alunoModel = AlunoDao.getInstance().getById(1);    
+		//AlunoModel alunoModel = AlunoDao.getInstance().getById(1);
+  
+		
+		
 	}	
 }
--- src/main/java/carregabanco/view/CarregaBancoView.java
+++ src/main/java/carregabanco/view/CarregaBancoView.java
@@ -8,7 +8,7 @@ public class CarregaBancoView {
 	public CarregaBancoView() {
 		CarregaBancoController carregaBancoController = new CarregaBancoController();
 		ArrayList<AlunoModel> valores = carregaBancoController.loader("alunostads.csv");
-		//carregaBancoController.inserirNoBanco(valores);
+		carregaBancoController.inserirNoBanco(valores);
 		
 		//A parte da visualização seria isso
 		System.out.println(valores.size());
